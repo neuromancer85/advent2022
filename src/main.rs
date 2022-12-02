53e5e21a91329159fe9fab7cc02b0b6ffe7deabb
Antonio Bonifacio <antonio.bonifacio@telepass.com>
2022-12-02 10:02:51 +0100

    "Advent 2022, day 1: more cleanup"

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 fn advent_day_1() {
     println!("Day 1 -------");
     let contents = fs::read_to_string("src/calories.txt").expect("Cannot open file");
-    let summed_calories: Vec<u32> = contents
+    let mut elves_vec: Vec<u32> = contents
         .split("\n\n")
         .map(|group| {
             group.lines().fold(0, |acc, line| {
@@ -16,15 +16,10 @@ fn advent_day_1() {
         })
         .collect();
 
-    let mut elves_vec = vec![];
-    for &sum in summed_calories.iter() {
-        elves_vec.push(sum);
-    }
-
-    elves_vec.sort();
+    elves_vec.sort_by(|a, b| b.cmp(a));
     println!("Elf with more calories: {:?}", elves_vec.last());
 
-    let sum_top_3 = elves_vec.iter().rev().take(3).fold(0, |acc, i| acc + i);
+    let sum_top_3: u32 = elves_vec.iter().take(3).sum();
     println!("Sum of top 3 elves: {sum_top_3}");
     println!("-------------");
 }
